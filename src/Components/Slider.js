1758bb6681e393389d0898ca4e3b7080112e4027
Shyslo Liubou <slv5555555@mail.ru>
2022-09-05 23:41:00 +0300

    changes

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -14,9 +14,9 @@ import three from '../slider3.jpg';
         <div className='sliderAll'>
         
    <Carousel>
-    <Carousel.Item style={{'height': '800px'}}>
-        <img className='d-block w-100'
-        src={one} alt='photo 1' />
+    <Carousel.Item style={{'height': '1000px'}}>
+    <img className='d-block w-100'
+        src={two} alt='photo 2' />
         <Carousel.Caption style={{'color': '#000000', 'font-weight': 'bold', 'font-size': '20px'}}>
             <h3 className='sliderShift'>iodo-band</h3>
             <p className='sliderShift'>31 августа</p>
@@ -24,16 +24,16 @@ import three from '../slider3.jpg';
             <p className='sliderShift'>22.00</p>
         </Carousel.Caption>
     </Carousel.Item>
-    <Carousel.Item style={{'height': '800px'}}>
+    <Carousel.Item style={{'height': '1000px'}}>
         <img className='d-block w-100'
-        src={two} alt='photo 2' />
+        src={one} alt='photo 1' />
         <Carousel.Caption style={{ 'text': 'center','color': '#000000', 'font-weight': 'bold', 'font-size': '20px'}}>
             <h3 className='sliderShift'>iodo-band</h3>
             <p className='sliderShift'>Это топовый коллектив на любой ваш праздник: свадьбу, юбилей, Новый год</p>
 
         </Carousel.Caption>
     </Carousel.Item>
-    <Carousel.Item style={{'height': '800px'}}>
+    <Carousel.Item style={{'height': '1000px'}}>
         <img className='d-block w-100'
         src={three} alt='photo 1' />
         <Carousel.Caption style={{'font-weight': 'bold', 'font-size': '20px'}}>
